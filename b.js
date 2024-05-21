de371748f7203f6266d42dd45b51840a66f2fe38
unknown <jabirmustafasulaiman@gmail.com>
2024-05-21 16:40:46 -0700

    my first website

---
+++ b.js
@@ -0,0 +1,8 @@
+let navbarBtn = document.getElementById("toggleBtn");
+let navbarNav = document.getElementById("collapse");
+
+navbarBtn.addEventListener("click", function(e) {
+
+    navbarNav.classList.toggle("show")
+
+});
---
+++ script.js
@@ -0,0 +1,7 @@
+
+    const toggleBtn = document.getElementById('toggleBtn');
+    const navbarNav = document.getElementById('collapse');
+
+    toggleBtn.addEventListener('click', ()=> {
+        navbarNav.classList.toggle('show');
+    });
